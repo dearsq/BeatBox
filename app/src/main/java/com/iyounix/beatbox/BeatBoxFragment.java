cba8221c3115bc57f16b19d2095c3003323652f9
章扬 <foreveryounix@gmail.com>
2018-12-13 14:52:46 +0800

    Initial commit

---
+++ app/src/main/java/com/iyounix/beatbox/BeatBoxFragment.java
@@ -0,0 +1,15 @@
+package com.iyounix.beatbox;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+//        Bundle args = new Bundle();
+//
+//        BeatBoxFragment fragment = new BeatBoxFragment();
+//        fragment.setArguments(args);
+        return new BeatBoxFragment();
+    }
+}
