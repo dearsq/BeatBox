36dec95da491201847027b953fc3f762ceddda5e
章扬 <foreveryounix@gmail.com>
2018-12-14 11:05:03 +0800

    [Function] 添加 button 和 Adapter

--- app/src/main/java/com/iyounix/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/iyounix/beatbox/BeatBoxFragment.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.iyounix.beatbox.databinding.FragmentBeatBoxBinding;
+import com.iyounix.beatbox.databinding.ListItemSoundBinding;
 
 public class BeatBoxFragment extends Fragment {
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
     }
@@ -21,6 +25,38 @@ public class BeatBoxFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //实例化绑定类
         FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        //通过数据绑定获取视图
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        binding.recyclerView.setAdapter(new SoundAdapter());
+
         return binding.getRoot();
+    }
+
+    //
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        public SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil.inflate(inflater,R.layout.list_item_sound,parent,false);
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SoundHolder soundHolder, int position) {
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
