1f43862048ac7761d4526b1ee503cd2df9a35e87
章扬 <foreveryounix@gmail.com>
2018-12-14 09:57:42 +0800

    [Function] 改用数据绑定, 完成实例化绑定类

--- app/src/main/java/com/iyounix/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/iyounix/beatbox/BeatBoxFragment.java
@@ -1,9 +1,26 @@
 package com.iyounix.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.iyounix.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //实例化绑定类
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
+        return binding.getRoot();
     }
 }
