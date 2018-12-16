e49b0be107323167c1a269af70246c8d1ae6539a
章扬 <foreveryounix@gmail.com>
2018-12-16 20:49:32 +0800

    [Function] 釋放 BeatBox 聲音資源

--- app/src/main/java/com/iyounix/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/iyounix/beatbox/BeatBoxFragment.java
@@ -87,4 +87,10 @@ public class BeatBoxFragment extends Fragment {
             return mSounds.size();
         }
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
+    }
 }
