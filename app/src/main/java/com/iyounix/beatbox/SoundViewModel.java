72b5f02d27ee1480695f184a9216952f3fd5107f
章扬 <foreveryounix@gmail.com>
2018-12-17 14:49:25 +0800

    [Function] 使用BaseObservable类：        (1) 在视图模型里继承BaseObservable类；        (2) 使用@Bindable注解视图模型里可绑定的属性；        (3) 每次可绑定的属性值改变时，就调用notifyChange()方法或notifyPropertyChanged(int)方法。

--- app/src/main/java/com/iyounix/beatbox/SoundViewModel.java
+++ app/src/main/java/com/iyounix/beatbox/SoundViewModel.java
@@ -1,11 +1,14 @@
 package com.iyounix.beatbox;
 
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * 1. 完成对 Sound 回调和监听器的设置
  * 2. 对 list_item_sound.xml 是可见属性
  */
-public class SoundViewModel {
+public class SoundViewModel extends BaseObservable {
     // adapter 要用的接口
     private Sound mSound;
     private BeatBox mBeatBox;
@@ -20,8 +23,14 @@ public class SoundViewModel {
 
     public void setSound(Sound sound) {
         mSound = sound;
+        //通知绑定类,ViewModel对象上所有可绑定属性都已更新
+        notifyChange();
+        //通知绑定类,ViewModel对象上只有 getTitle() 方法的值有变化
+        //notifyPropertyChanged();
+
     }
 
+    @Bindable
     public String getTitle() {
         return mSound.getName();
     }
